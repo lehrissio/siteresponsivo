dc8cbb8f158e5be55fd800c930c91c3ada113f7c
Leticia Rissio Silva <103052994+lehrissio@users.noreply.github.com>
2022-04-05 13:01:13 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,12 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 function scrollHeader(){
     const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
@@ -40,7 +38,6 @@ linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
